01b4e9287bcd61cf569a676a4dec90b3a8465a74
excel <adeniyiexcel@gmail.com>
2026-09-22 11:25:24 +0100

    Refactor main function to use a loop and switch statement for better user interaction

--- bank.go
+++ bank.go
@@ -4,49 +4,56 @@ import "fmt"
 
 func main() {
 	accountBalance := 1000.0
-	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
 
-	var choice int
+	for {
+		fmt.Println("Welcome to Go Bank!")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
 
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
+		var choice int
 
-	if choice == 1 {
-		fmt.Println("Your balance is", accountBalance)
-	} else if choice == 2 {
-		fmt.Print("Your deposit: ")
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is", accountBalance)
+		case 2:
+			fmt.Print("Your deposit: ")
 
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
-		if depositAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-		}
-		accountBalance += depositAmount
-		fmt.Println("Balance updated! New amount:", accountBalance)
-	} else if choice == 3 {
-		fmt.Print("Withdraw Amount: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+			}
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New amount:", accountBalance)
+		case 3:
+			fmt.Print("Withdraw Amount: ")
 
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-		if withdrawAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+
+			if withdrawAmount > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you hav.")
+				continue
+			}
+			accountBalance -= withdrawAmount
+
+			fmt.Println("Balance updated! New amount:", accountBalance)
+		default:
+			fmt.Println("Goodbye!")
+			fmt.Println("Thanks for choosing our bank")
 			return
 		}
-
-		if withdrawAmount > accountBalance {
-			fmt.Println("Invalid amount. You can't withdraw more than you hav.")
-			return
-		}
-		accountBalance -= withdrawAmount
-
-		fmt.Println("Balance updated! New amount:", accountBalance)
-	} else {
-		fmt.Println("Goodbye!")
+		
 	}
 
+	
 }
